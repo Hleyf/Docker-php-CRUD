b2df5ca34830ac4643a408c90e365f51076cefb1
Hleyf <hleyf@hotmail.com>
2024-03-07 13:44:30 +0000

    Refactor code for login functionality and improve code readability

--- php/crud/login_user.php
+++ php/crud/login_user.php
@@ -3,15 +3,18 @@
 
     $userName = $password = "";
 
-    //form validation
     if ($_SERVER["REQUEST_METHOD"] == "GET"){
         
         validateForm($userName, $password);
 
-        $query = "SELECT * FROM user WHERE userName = '$userName'";
-        $result = mysqli_query($db_connection, $query);
-        if(mysqli_num_rows($result) == 1){
-            validateUserPassword($result, $password);
+        if(checkUserExist($db_connection, $userName)){
+            $validUser = validateUserPassword($result, $password);
+
+            //user is stored in session after being validated. 
+            //TODO: "remmember me" is not implemented since user won't persist after session ends.
+            $_SESSION["user"] = $validUser; 
+
+            header("Location: ../../pages/task/task_main.php");
         }else {
             $_SESSION["userNameErrMessage"] = "User Name does not exist";
             backToLogin();
@@ -44,14 +47,21 @@
             $user->id = $row['id'];
             $user->userName = $row['user_name'];
             
-            //user is stored in session after being validated. 
-            //TODO: "remmember me" is not implemented since user won't persiste after session ends.
-            $_SESSION["user"] = $user; 
+            return $user;
 
-            header("Location: ../../pages/task/task_main.php");
         }else{
             $_SESSION["passwordErrMessage"] = "Password is incorrect";
             backToLogin();
+        }
+    }
+    // Check if the user exists in the database.
+    function checkUserExist(&$db_connection, &$userName){
+        $query = "SELECT * FROM user WHERE userName = '$userName'";
+        $result = mysqli_query($db_connection, $query);
+        if(mysqli_num_rows($result) == 1){
+            return true;
+        }else{
+            return false;
         }
     }
 
@@ -62,7 +72,8 @@
         $data = htmlspecialchars($data);
         return $data;
     }
-    // Just an easy way to redirect to the login page in case of error. Error messages must be stored in the session before calling it.
+    // Just an easy way to redirect to the login page in case of error. 
+    // Error messages must be stored in the session before calling it.
     function backToLogin(){
         header("Location: ../../pages/auth/login.php");
     }
